b83c77e63336751880d0b9a4d70df2b15ea63a65
Lovedeep <lovedeep.lovedeep@capgemini.com>
2022-08-30 18:57:55 +0530

    pushed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   var clientIp = requestIp.getClientIp(req);
   res.json({ ip: clientIp });
 });

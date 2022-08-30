b2faf4802a8ed0b6d1df6ede52b4a51fc1de3503
Lovedeep <lovedeep.lovedeep@capgemini.com>
2022-08-30 18:39:39 +0530

    another cahnge

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 var requestIp = require("request-ip");
 
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", function (req, res) {
   var clientIp = requestIp.getClientIp(req);
